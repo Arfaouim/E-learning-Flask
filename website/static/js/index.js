408e28afba2a7f4a69d2caf5d63c0688ee5fb5ef
Arfaouim <arfaouimehdi20@yahoo.fr>
2021-08-28 09:52:50 +0200

    commit

--- website/static/js/index.js
+++ website/static/js/index.js
@@ -76,3 +76,23 @@ btn.on('click', function(e) {
 });
 
 
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    dots:false,
+    nav:true,
+    autoplay:true,   
+    smartSpeed: 3000, 
+    autoplayTimeout:3000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
